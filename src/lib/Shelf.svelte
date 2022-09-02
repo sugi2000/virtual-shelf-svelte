c422657c2d668733c9f89673d5ef87432a8be919
sugi2000 <sugi2000@gmail.com>
2022-09-02 11:05:20 +0900

    1 file added

---
+++ src/lib/Shelf.svelte
@@ -0,0 +1,18 @@
+<script>
+	import * as d3 from 'd3';
+  import Spine from 'Spine.svelte';
+
+	export let data;
+</script>
+
+<div class="book-shelf">
+{#each d as data}
+  <Spine data={d}/>
+{/each}
+</div>
+
+<style>
+.book-shelf {
+	display: flex;
+}
+</style>
