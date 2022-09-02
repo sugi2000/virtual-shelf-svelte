437a99579d72635f8abbed466006369bbaf5694c
sugi2000 <sugi2000@gmail.com>
2022-09-02 11:51:34 +0900

    1 file modified

--- src/lib/Spine.svelte
+++ src/lib/Spine.svelte
@@ -44,7 +44,7 @@
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" 
 	width="30" height="400" class="book-spine">
 		<rect width="30" height="400" fill="#800"></rect>
-		<text x="0" y="50" font-family="sans-serif" fill="#fff>{isbn}text>
+		<text x="0" y="50" font-family="sans-serif" fill="#fff>{isbn}<text>
 </svg>
 {:then result}
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" 
