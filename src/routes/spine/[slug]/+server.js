15b9e9b4acd80e6b5e31775406765c5f0b157a16
sugi2000 <sugi2000@gmail.com>
2022-09-05 14:58:46 +0900

    1 file modified

--- src/routes/spine/[slug]/+server.js
+++ src/routes/spine/[slug]/+server.js
@@ -8,7 +8,7 @@ export function GET({ params }) {
 	const isbn = slug.at(0);
 	// console.log('isbn:', isbn);
 
-	searchISBN(isbn).then((info) => console.log(info));
+	// searchISBN(isbn).then((info) => console.log(info));
 
 	// const svg = d3
 	// 	.create('svg')
