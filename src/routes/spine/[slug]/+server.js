c497921befa23838863a55d812367783f67cafa2
sugi2000 <sugi2000@gmail.com>
2022-09-05 14:48:34 +0900

    1 file modified

--- src/routes/spine/[slug]/+server.js
+++ src/routes/spine/[slug]/+server.js
@@ -21,8 +21,8 @@ export function GET({ params }) {
 
 	// svg.append('g').append('rect').attr('width', 30).attr('height', 420).attr('fill', '#888');
 
-	const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100" height="100">
-		<rect width="100" height="100" fill="#000"></rect>
+	const svg = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" width="100" height="100" class="spine">
+		<rect width="420" height="300" fill="#000"></rect>
 		<text x="0" y="50" font-family="sans-serif" fill="#fff">${isbn}</text>
 	</svg>`;
 
