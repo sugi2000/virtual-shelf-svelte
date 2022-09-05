b63c75f62f038346cc72ff9431ab3b175a0ee9a0
sugi2000 <sugi2000@gmail.com>
2022-09-05 15:22:44 +0900

    1 file modified

--- src/lib/Spine.svelte
+++ src/lib/Spine.svelte
@@ -1,9 +1,12 @@
 <script>
+	import { onMount } from 'svelte';
 	import * as d3 from 'd3';
 	// import x2js from 'x2js';
 	export let data;
 	// import { page } from '$app/stores';
 	// import {searchISBN} from './search.js';
+
+	let el;
 
 	$: isbn = data.isbn;
 
@@ -42,7 +45,7 @@
 
 
 		const xml = (new window.DOMParser()).parseFromString(openXMLText, "text/xml");
-		console.log(xml.querySelector('rss channel item title'));
+		// console.log(xml.querySelector('rss channel item title'));
 		return {
 			title: xml.querySelector('rss channel item title').textContent
 		};
@@ -54,9 +57,30 @@
 
 	$: item = searchISBN(isbn);
 
+	onMount(async () => {
+		const item = await searchISBN(isbn);
+		const svg = d3.select(el)
+			.append('svg')
+			.attr('xmlns', 'http://www.w3.org/2000/svg')
+			.attr('xmlns:xlink', 'http://www.w3.org/1999/xlink')
+			.attr('version', 1.1)
+			.attr('width', 30)
+			.attr('height', 420);
+		svg.append('rect')
+			.attr('width', 30)
+			.attr('height', 420)
+			.attr('fill', '#666');
+		svg.append('text')
+			.attr('x', 0).attr('y', 20)
+			.attr('fill', '#fff')
+			.text(item.title);
+	});
+
 </script>
 
-{#await item}
+<div bind:this={el} class="book-spine"></div>
+
+<!-- {#await item}
 <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" 
 	width="30" height="400" class="book-spine">
 		<rect width="30" height="400" fill="#666"></rect>
@@ -74,14 +98,15 @@
 		<rect width="30" height="400" fill="#800"></rect>
 		<text x="0" y="50" font-family="sans-serif" fill="#fff">{`error`}</text>
 </svg>
-{/await}
+{/await} -->
 
 <style>
 svg {
 	display: inline-block;
 	margin: 1px;
 }
-svg text {
+svg text,
+.book-spine text {
 	writing-mode: vertical-rl;
 }
 </style>
